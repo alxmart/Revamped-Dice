40da98494ff4107e8cf753a0ecfd0a4373d484b1
alxmart <alxmartinez@gmail.com>
2022-08-06 12:16:30 -0300

    Enabling viewBinding and added code to roll the dice.

--- app/src/main/java/com/example/revampeddice/MainActivity.kt
+++ app/src/main/java/com/example/revampeddice/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.revampeddice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.revampeddice.databinding.ActivityMainBinding
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRoll.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = binding.tvNumber
+        resultTextView.text = diceRoll.toString()
     }
 }
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
+
+
+
 
